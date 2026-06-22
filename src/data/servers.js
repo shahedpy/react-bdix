c74686a461bde66c14a4e9ef81dcdf38c40446dc
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2026-06-23 00:39:40 +0600

    servers list

---
+++ src/data/servers.js
@@ -0,0 +1,16 @@
+export const servers = [
+  {
+    id: 1,
+    name: "Example Server",
+    url: "http://example.com",
+    testUrl: "http://example.com/favicon.ico",
+    category: "FTP / Movie"
+  },
+  {
+    id: 2,
+    name: "Another Server",
+    url: "http://example2.com",
+    testUrl: "http://example2.com/favicon.ico",
+    category: "Game"
+  }
+];
